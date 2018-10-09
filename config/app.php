7cb14f9721b6bec7aa7045b3c7fa8c5f1cdbf476
Ben Gurney <ben@webhappens.co.uk>
2018-10-09 16:54:16 +0100

    Register Nova tool

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        WebHappens\Questions\Nova\ToolServiceProvider::class,
 
         /*
          * Application Service Providers...
